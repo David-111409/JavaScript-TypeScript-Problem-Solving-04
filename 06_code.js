08608438e6a7592222dfce37d9d1e59a99dc5141
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-13 02:45:43 +0300

    Update 06_code.js

--- 06_code.js
+++ 06_code.js
@@ -22,3 +22,40 @@ console.log(isFloatingCharacter("av0.12"), false);
 console.log(isFloatingCharacter("-."), false);
 console.log(isFloatingCharacter("+"), false);
 console.log(isFloatingCharacter("12"), false);
+
+
+/*
+ retrieve("A simple life is a happy life for me.") ➞ ["a", "is", "a"]
+
+retrieve("Exercising is a healthy way to burn off energy.")
+➞ ["exercising", "is", "a", "off", "energy"]
+
+retrieve("The poor ostrich was ostracized.")
+➞ ["ostrich", "ostracized"]
+
+retrieve("")
+➞ []
+ */
+
+function retrieve(sent) {
+  let re = /\b[aeiou]\w*\b/gi;
+  //   return sent.toLowerCase().match(re) || [];
+  return sent.match(re)?.map((word) => word.toLowerCase()) || [];
+}
+console.log(retrieve("A simple life is a happy life for me."), [
+  "a",
+  "is",
+  "a",
+]);
+console.log(retrieve("Exercising is a healthy way to burn off energy."), [
+  "exercising",
+  "is",
+  "a",
+  "off",
+  "energy",
+]);
+console.log(retrieve("The poor ostrich was ostracized."), [
+  "ostrich",
+  "ostracized",
+]);
+console.log(retrieve(""), []);
