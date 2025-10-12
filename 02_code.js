2780748eea995e2f46b21391860c97f26679e5d1
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-12 21:51:39 +0300

    Update 02_code.js

--- 02_code.js
+++ 02_code.js
@@ -39,3 +39,31 @@ console.log(validateEmail("hello.gmail@com"));   // false
 console.log(validateEmail("gmail"));             // false
 console.log(validateEmail("hello@gmail"));       // false
 console.log(validateEmail("hello@edabit.com"));  // true
+
+
+/*
+mathExpr("4 + 5") ➞ true
+
+mathExpr("4*6") ➞ true
+
+mathExpr("4*no") ➞ false
+*/
+function mathExpr(exp) {
+  return /^\d+\s*[*/%+-]\s*\d+$/i.test(exp);
+}
+
+// console.log(mathExpr("234  * 3234"));
+// console.log(mathExpr("5+4"), true);
+// console.log(mathExpr("4 * 5"), true);
+// console.log(mathExpr("3*6"), true);
+// console.log(mathExpr("4 - 5"), true);
+// console.log(mathExpr("6 % 7"), true);
+// console.log(mathExpr("a - b"), false);
+// console.log(mathExpr("a - 2"), false);
+// console.log(mathExpr("nope"), false);
+
+console.log(/^\d+\s?[*/%+-]\s?\d+$/.test("4+5"));      // true
+console.log(/^\d+\s?[*/%+-]\s?\d+$/.test("12 * 3"));   // true
+console.log(/^\d+\s?[*/%+-]\s?\d+$/.test("7 -9"));     // true
+console.log(/^\d+\s?[*/%+-]\s?\d+$/.test("4 * no"));   // false
+console.log(/^\d+\s?[*/%+-]\s?\d+$/.test("8//2"));     // false
