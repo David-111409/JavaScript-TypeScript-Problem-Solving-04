f1caada86392d376d3e01c6070c94b0da6dbd5e1
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-12 21:13:20 +0300

    Update 01_code.js

--- 01_code.js
+++ 01_code.js
@@ -13,3 +13,39 @@ let urls = "https://google.com http://www.website.net web.com"; // http + https
 let re = /(https?:\/\/)?(www\.)?\w+\.(net|com)/ig;
 
 console.log(urls.match(re));
+
+/**
+ XO("ooxx") ➞ true
+
+XO("xooxx") ➞ false
+
+XO("ooxXm") ➞ true
+// Case insensitive.
+
+XO("zpzpzpp") ➞ true
+// Returns true if no x and o.
+
+XO("zzoo") ➞ false
+ */
+
+function XO(str) {
+  
+  let x = str.match(/x/gi)?.length || 0;
+  let o = str.match(/o/gi)?.length || 0;
+
+  return x === o;
+}
+
+console.log(XO("ooxx"), true);
+console.log(XO("xooxx"), false);
+
+console.log(XO("ooxXm"), true);
+console.log(XO("zpzpzpp"), true);
+console.log(XO("zzoo"), false);
+
+console.log(XO("Xo"), true);
+console.log(XO("x"), false);
+console.log(XO("o"), false);
+
+console.log(XO("xxxoo"), false);
+console.log(XO(""), true);
