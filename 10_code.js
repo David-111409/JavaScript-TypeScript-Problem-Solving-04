708e7b7774329fabe8540fe356f43d6481dc4944
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-13 06:05:36 +0300

    Update 10_code.js

--- 10_code.js
+++ 10_code.js
@@ -23,3 +23,33 @@ const validate = (REGEXP) => {
 const testExp = validate(REGEXP);
 
 console.log(testExp(str), ["-1.5", "0", "2", "-123.4"]);
+
+/*
+Cluster Vowels, Single Out Consonants
+Write a function that takes in a word and splits the consonants one by one, but keeps the vowels in a cluster.
+
+Examples
+
+split("beautifully") ➞ ["b", "eau", "t", "i", "f", "u", "l", "l", "y"]
+
+split("spoonful") ➞ ["s", "p", "oo", "n", "f", "u", "l"]
+
+split("swimming") ➞ ["s", "w", "i", "m", "m", "i", "n", "g"]
+ */
+const split = (str) => str.match(/([aeiou]+)|[^aeiou]/g);
+
+console.log(split("beautifully"), [
+  "b",
+  "eau",
+  "t",
+  "i",
+  "f",
+  "u",
+  "l",
+  "l",
+  "y",
+]);
+console.log(split("spoonful"), ["s", "p", "oo", "n", "f", "u", "l"]);
+console.log(split("swimming"), ["s", "w", "i", "m", "m", "i", "n", "g"]);
+console.log(split("courage"), ["c", "ou", "r", "a", "g", "e"]);
+console.log(split("cooing"), ["c", "ooi", "n", "g"]);
