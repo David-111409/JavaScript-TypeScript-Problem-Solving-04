f0ddff004a29866a80be6113d651f3233eba0d67
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-14 05:00:43 +0300

    Update 16_code.js

--- 16_code.js
+++ 16_code.js
@@ -12,3 +12,46 @@ console.log(url3.match(re));
 console.log(url4.match(re));
 console.log(url5.match(re));
 console.log(url6.match(re));
+
+/*
+Extend the Vowels
+Create a function that takes a word and extends all vowels by a number num.
+
+Examples
+
+extendVowels("Hello", 5) ➞ "Heeeeeelloooooo"
+
+extendVowels("Edabit", 3) ➞ "EEEEdaaaabiiiit"
+
+extendVowels("Extend", 0) ➞ "Extend"
+Notes
+
+Return "invalid" if num is not a positive integer or 0.
+*/
+
+const extendVowels = (str, n) => {
+    if (n < 0 || !Number.isInteger(n)) {
+        return "invalid";
+    } else if (n === 0) {
+        return str;
+    } else {
+        // n++;
+        return str.replace(/[aeiou]/gi, (el) => `${el.repeat(n + 1)}`);
+    }
+};
+
+console.log(extendVowels("hello", 3));
+
+console.log(extendVowels("Hello", 5) === "Heeeeeelloooooo");
+
+console.log(extendVowels("Edabit", 3) === "EEEEdaaaabiiiit");
+
+console.log(extendVowels("Extend", 0) === "Extend");
+
+console.log(extendVowels("A", 10) === "AAAAAAAAAAA");
+
+console.log(extendVowels("Z", 93) === "Z");
+
+console.log(extendVowels("Vowel", 0.5) === "invalid");
+
+console.log(extendVowels("Nice", -8) === "invalid");
