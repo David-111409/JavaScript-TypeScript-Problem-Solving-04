cd139e91638f9a8ac23ce618fcb9f882fc542e52
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-15 22:24:15 +0300

    Add examples for logical and arithmetic operations

--- 26_code.js
+++ 26_code.js
@@ -2,3 +2,9 @@ console.log(null || false || "hello"); // "hello"
 console.log(null && false && "hello"); // null
 console.log(false && null && "hello"); // false
 console.log(null || true || "hello"); // true
+
+let x = [1];
+let y = [2];
+
+console.log(x + y, typeof (x + y)); //"12", string
+console.log(+x + +y, typeof (+x + +y)); //3, number
