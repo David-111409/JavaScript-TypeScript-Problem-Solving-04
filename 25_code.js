8b23ef5a19b5875b11d0ec819e1aae6355ebcd2c
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-15 21:58:18 +0300

    Create 25_code.js

---
+++ 25_code.js
@@ -0,0 +1,13 @@
+{
+  var a = 2;
+  let b = 3;
+  const c = 4;
+}
+console.log(a); // output: 2
+//console.log(b, c); // error, error
+if ([].length) {
+  console.log("Hello"); // output: no output
+}
+if ([]) {
+  console.log("true"); // output : "true"
+}
