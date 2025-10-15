3b6eff13fa5cdbe4998e02dd138ca48382483518
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-15 05:24:15 +0300

    Create 22_code.js

---
+++ 22_code.js
@@ -0,0 +1,31 @@
+class User {
+  constructor(id, username, salary) {
+    this.i = id;
+    this.u = username;
+    this.s = salary;
+  }
+
+  updateName(newName) {
+    this.u = newName;
+  }
+}
+
+let userOne = new User(100, "Hager", 2000);
+
+console.log(userOne.u);
+userOne.updateName("Khaled");
+console.log(userOne.u);
+
+// let one = "Hager";
+// let two = new String("Hager");
+// console.log(one.constructor === two.constructor);
+// console.log(one instanceof String, two instanceof String);
+// console.log(typeof one, typeof two);
+// console.log(one.constructor === String, two.constructor === String);
+
+let one = 100;
+let two = new Number(100);
+console.log(one.constructor === two.constructor); // Number === Number
+
+console.log(one instanceof Number, two instanceof Number); // fales, true
+console.log(typeof one, typeof two); // number, object
