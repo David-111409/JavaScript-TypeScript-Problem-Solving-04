f432bcf8a08d08dd87fe7f70468f71a3515cb241
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-16 01:41:11 +0300

    Update 28_code.js

--- 28_code.js
+++ 28_code.js
@@ -28,3 +28,36 @@ console.log(typeof Math); // "object"
 console.log(typeof JSON); // "object"
 console.log(typeof arguments); // "object"
 console.log(typeof Promise.resolve()); // "object"
+
+let x = 123;
+let y = new Number(123);
+console.log(x === y); // false
+
+let str = "hello";
+let another = new String("hello");
+console.log(str === another, str == another); // false true
+console.log(typeof str); // "string"
+console.log(typeof another); // "object"
+
+console.log(another instanceof String); // true
+console.log(another instanceof Object); // true
+console.log(str instanceof String); // false
+
+console.log(typeof String, typeof Number, typeof Boolean); // "function function function"
+console.log(typeof Math, typeof JSON); // "object object"
+
+console.log(+null, Number(null), +NaN); // 0 0 NaN
+console.log(+""); // 0
+console.log(+"123"); // 123
+
+console.log(+"123abc"); // NaN
+console.log(+true); // 1
+console.log(+false); // 0
+
+console.log(+undefined); // NaN
+console.log(+{}); // NaN
+console.log(+[]); // 0
+
+console.log(+[123]); // 123
+console.log(+[1, 2, 3]); // NaN
+console.log(+function(){}); // NaN
