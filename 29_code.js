0a8f9c3f637c2515b491fb59f9b1ad3291429ffc
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-16 06:18:33 +0300

    Update 29_code.js

--- 29_code.js
+++ 29_code.js
@@ -31,3 +31,29 @@ console.log(x); // 11
 
 const y = "20";
 console.log(y++); // error
+
+let ar = [1, 2, 3, 4];
+console.log(1 in ar); // true
+console.log(2 in ar); // true
+console.log(3 in ar); // true
+console.log(4 in ar); // false
+console.log(5 in ar); // false
+console.log(0 in ar); // true
+
+{
+  let x = "8";
+  let a = 1;
+  let res = x + a + 1;
+  console.log(res, typeof res); // "811"  string
+
+  // What is the difference  between find and filter in js?
+  // find returns the first element that satisfies the condition,
+  // while filter returns all elements that satisfy the condition in an array.
+
+  let arr = [1, 2, 3, 4, 5, 6];
+  let findResult = arr.find((num) => num > 3); // 4
+  let filterResult = arr.filter((num) => num > 3); // [4, 5, 6]
+
+  console.log(findResult);
+  console.log(filterResult);
+}
