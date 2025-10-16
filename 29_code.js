885d258683c89e9844f285dd5baac48fff248cc6
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-16 05:39:02 +0300

    Add examples of Number.isNaN and loop behavior

---
+++ 29_code.js
@@ -0,0 +1,26 @@
+console.log(typeof Number.isNaN); // "function"
+console.log(Number.isNaN(NaN)); // true
+console.log(Number.isNaN(5)); // false
+console.log(Number.isNaN("NaN")); // false
+console.log(Number.isNaN(undefined)); // false
+
+let ar = [1, 2];
+for (var i = 0; i < ar.length; i++) {
+  // output : 2 2
+  setTimeout(
+    function () {
+      console.log(i);
+    },
+    [100]
+  );
+}
+
+for (let i = 0; i < ar.length; i++) {
+  // output : 0 1
+  setTimeout(
+    function () {
+      console.log(i);
+    },
+    [100]
+  );
+}
