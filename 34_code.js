8defe59c763be6e1d99c074e9204fb820c41a9f9
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-17 03:47:27 +0300

    Update 34_code.js

--- 34_code.js
+++ 34_code.js
@@ -6,3 +6,24 @@
   console.log([1] + [], typeof ([1] + [])); // "1" string
   console.log([1] + "abc"); // "1abc"
 }
+function theGreatestNumber(ar) {
+  let first = -Infinity;
+  let second = -Infinity;
+  let third = -Infinity;
+  let l = ar.length;
+  for (let i = 0; i < l; i++) {
+    if (ar[i] > first) {
+      third = second;
+      second = first;
+      first = ar[i];
+    } else if (ar[i] > second && ar[i] != first) {
+      third = second;
+      second = ar[i];
+    } else if (ar[i] > third && ar[i] != second && ar[i] != first) {
+      third = ar[i];
+    }
+  }
+  return [first, second, third];
+}
+console.log(theGreatestNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+//[10, 9, 8];
