d046ab1a1c43f6dab846f77787f2ef3d27b7a982
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-17 03:28:24 +0300

    Update 33_code.js

--- 33_code.js
+++ 33_code.js
@@ -18,3 +18,23 @@ console.log(a, b); // [1, 2, 3 ] [0, 2, 3]
   console.log(nums, nums.length); //array = [1, empty, 3, 4] , length = 4
 }
 
+{
+  let nums = [1, 2, 3].push(2, 3);
+  console.log(nums);
+
+  let x = [20, 1, 3].sort(); // [1, 20, 3]
+  let rev = [2, 1, 3].reverse();
+
+  console.log(x, rev);
+  console.log(1 + false); // 1
+  console.log(1 + true); // 2
+  console.log(1 - false); // 1
+  console.log(1 - true); // 0
+  console.log(1 + null); // 1
+
+  let arr = [1, 2, 3];
+  let str = "1,2,3";
+  console.log(arr == str); // true
+  console.log(arr === str); // false
+}
+
