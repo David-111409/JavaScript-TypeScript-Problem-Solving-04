e18d2a88e3db4d47b5c08972a1aa222680d520e7
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-17 06:55:25 +0300

    Update 35_code.js

--- 35_code.js
+++ 35_code.js
@@ -50,3 +50,41 @@ console.log(admin1.sayHello()); // Hello Bob
 console.log(superOne.sayHello()); // Hello Charlie
 
 console.log(superOne.i); // 3
+
+
+function isNarcissistic(num) {
+  let len = num.toString().length;
+  let originalNum = num;
+  let sum = 0;
+
+  while (num) {
+    let digit = num % 10;
+
+    sum += digit ** len;
+
+    num = Math.floor(num / 10);
+  }
+
+  return sum === originalNum;
+}
+
+console.log(isNarcissistic(0), true, "0 is narcissistic");
+console.log(isNarcissistic(1), true, "1 is narcissistic");
+
+console.log(isNarcissistic(5), true, "5 is narcissistic");
+
+console.log(isNarcissistic(7), true, "7 is narcissistic");
+
+console.log(isNarcissistic(153), true, "153 is narcissistic");
+console.log(isNarcissistic(370), true, "370 is narcissistic");
+
+console.log(isNarcissistic(371), true, "371 is narcissistic");
+
+console.log(isNarcissistic(1634), true, "1634 is narcissistic");
+console.log(isNarcissistic(9004), false);
+
+console.log(isNarcissistic(2546), false);
+
+console.log(isNarcissistic(2124), false);
+
+console.log(isNarcissistic(8345), false);
