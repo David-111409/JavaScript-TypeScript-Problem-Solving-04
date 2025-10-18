e81d039ecf3ba2784f321cd7d8d257bdf439d2e0
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-18 11:32:39 +0300

    Update 43_code.js

--- 43_code.js
+++ 43_code.js
@@ -3,3 +3,21 @@ console.log(JSON.stringify(x)); // { "b" : null}
 
 let y = {c:NaN};
 console.log(JSON.stringify(y)); // {"c":null}
+
+let x = "5";
+let y = true;
+
+console.log(x + y); // "5true"
+console.log(typeof (x + y)); // "string" 
+console.log(x * y); // 5
+
+console.log(typeof (x * y)); // "number"
+console.log(x - y); // 4
+console.log(typeof (x - y)); // "number"
+
+console.log(+true); // 1
+console.log(+"123"); // 123 of number type
+console.log(+false); // 0
+
+console.log(+"hello"); // NaN
+console.log(+null); // 0
