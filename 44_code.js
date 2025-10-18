b76da82af959b86ed18c4f49be3785d9b6fc3864
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-19 01:47:44 +0300

    Update 44_code.js

--- 44_code.js
+++ 44_code.js
@@ -8,3 +8,17 @@ console.log(nameGen); // Hello, david!
     let count = 2;
     console.log(`Count is :: ${count}`);
 })(); // Count is: 0
+
+function makeRug(len, width, char = "#") {
+  //   let ar = [];
+  //   for (let i = 0; i < len; i++) {
+  //     ar[i] = char.repeat(width);
+  //   }
+  //   return ar;
+  return Array(len).fill(char.repeat(width));
+}
+
+console.log(makeRug(3, 5));
+console.log(makeRug(3, 5, "$"));
+console.log(makeRug(2, 2, "A"));
+console.log(makeRug(2, 1, "bcb"), ["bcb", "bcb"]);
