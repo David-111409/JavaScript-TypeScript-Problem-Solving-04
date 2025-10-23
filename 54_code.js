d737e053d98a3993bfc37a94201aeb1b8ff56396
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-23 16:31:24 +0300

    Update 54_code.js

--- 54_code.js
+++ 54_code.js
@@ -6,3 +6,21 @@ console.log(one, two); // outputs 1 2
 
 console.log(one, 1, "The variable one must equal 1");
 console.log(two, 2, "The variable two must equal 2");
+
+/*
+Write a function that returns true if an object is empty, and false otherwise.
+
+Examples
+
+isEmpty({}) ➞ true
+
+isEmpty({ a: 1 }) ➞ false
+*/
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+console.log(isEmpty({}), true);
+console.log(isEmpty({ a: 1 }), false);
+console.log(isEmpty({ z: 2, w: 4, y: 5 }), false);
