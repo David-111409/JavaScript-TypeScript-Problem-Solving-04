83c80811d693851132fb09a4cf67799dfc2d5e44
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-08 14:23:07 +0200

    Update 66_code.js

--- 66_code.js
+++ 66_code.js
@@ -34,3 +34,44 @@ function binarySearch(arr, target) {
 console.log(binarySearch([-5, 2, 4, 6, 10], 10));
 console.log(binarySearch([-5, 2, 4, 6, 10], 6));
 console.log(binarySearch([-5, 2, 4, 6, 10], 20));
+
+// function firstOccurence(ar, target) {
+//   let start = 0;
+//   let end = ar.length - 1;
+//   let first = -1;
+//   while (start <= end) {
+//     let mid = Math.floor((start + end) / 2);
+//     if (ar[mid] === target) {
+//       end = mid - 1;
+//       first = mid;
+//     } else if (ar[mid] < target) {
+//       start = mid + 1;
+//     } else {
+//       end = mid - 1;
+//     }
+//   }
+//   return first;
+// }
+// console.log(firstOccurence([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10], 20));
+
+function lastOccurence(ar, target) {
+  let start = 0;
+  let end = ar.length - 1;
+  let last = -1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (ar[mid] === target) {
+      start = mid + 1;
+      last = mid;
+    } else if (ar[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return last;
+}
+
+console.log(lastOccurence([1, 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
