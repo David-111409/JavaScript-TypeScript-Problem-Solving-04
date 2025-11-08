6a3a5fe0692eaff849d9eb97c1578dfba4c1fd1c
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-08 12:33:03 +0200

    Comment out object operations in 66_code.js

---
+++ 66_code.js
@@ -0,0 +1,15 @@
+// let obj = { name: "Khaled", age: 25 };
+
+// console.log(obj.name); // time : O(1)
+// delete obj.age; // time : O(1)
+// obj.hello = function () {
+//   return "Hello " + this.name; // time : O(1)
+// };
+// console.log(obj.hello()); // time : O(1)
+// for (let key in obj) {
+//   console.log(key); // time : O(n)
+// }
+
+// console.log(Object.keys(obj)); // time : O(n)
+// console.log(Object.values(obj)); // time : O(n)
+// console.log(Object.entries(obj)); // time : O(n)
