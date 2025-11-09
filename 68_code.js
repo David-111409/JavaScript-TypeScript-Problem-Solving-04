ce361f63316733968a9c534082c8dbfd3a507a82
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-09 17:03:08 +0200

    Implement binary search for square root calculation
    Added a function to calculate the square root using binary search.

--- 68_code.js
+++ 68_code.js
@@ -27,3 +27,28 @@ function binarySearch(ar, x, start = 0, end = ar.length - 1) {
   return binarySearch(ar, x, mid + 1, end);
 }
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1));
+
+function sqrt(n) {
+  let left = 0;
+  let right = n;
+  let mid;
+  let s;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    let res = mid * mid;
+    if (res <= n) {
+      left = mid + 1;
+      s = mid;
+    } else if (res > n) {
+      right = mid - 1;
+    }
+  }
+
+  return s;
+}
+console.log(sqrt(0));
+console.log(sqrt(1));
+console.log(sqrt(2));
+console.log(sqrt(3));
+console.log(sqrt(4));
