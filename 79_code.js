d0e7c34110798aee368f0fed273f3f18ff9805d9
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-14 14:13:30 +0200

    Add problem statement for 'Single Number II'
    Added a problem statement for 'Single Number II' and commented out the original function for finding a single number.

--- 79_code.js
+++ 79_code.js
@@ -28,3 +28,63 @@
 // console.log(rev("Hello, Wolrd!"));
 // console.log(rev("I love coding."));
 // console.log(rev("WoW! This is amazing!"));
+
+/*
+137. Single Number II
+Medium
+Topics
+premium lock iconCompanies
+
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 3 * 104
+    -231 <= nums[i] <= 231 - 1
+    Each element in nums appears exactly three times except for one element which appears once.
+
+*/
+
+// function singleNumber(ar) {
+//   let map = new Map();
+//   for (let i = 0; i < ar.length; i++) {
+//     map.set(ar[i], (map.get(ar[i]) || 0) + 1);
+//   }
+//   for (let [key, value] of map) {
+//     if (value === 1) {
+//       return key;
+//     }
+//   }
+// }
+
+// console.log(singleNumber([2, 2, 999, 2, 0, 0, 0]));
+
+var isValid = function (s) {
+  let matching = { ")": "(", "]": "[", "}": "{" };
+  let stack = [];
+  for (let e of s) {
+    if ("({[".includes(e)) {
+      stack.push(e);
+    } else if (stack.pop() !== matching[e]) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
+console.log(isValid("[()[][]()]"));
