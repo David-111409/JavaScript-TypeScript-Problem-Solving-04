de7a101124b39ff16a03c024cb2275353e974f38
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-16 19:22:07 +0200

    Update 80_code.js

--- 80_code.js
+++ 80_code.js
@@ -10,3 +10,25 @@ var majorityElement = function (nums) {
     return cand;
 };
 console.log(majorityElement([2, 3, 2, 2]));
+
+// console.log(+!!NaN * ""- -[,]); // false => 0 * "" => 0
+
+// console.log(!!" " * 1); // true => 1 * 1 => 1
+// console.log(!!" " * 1); // true => 1 * 1 => 1
+
+String.prototype.camelCase = function () {
+    //your code here
+    return this.replace(/^\w|\s([a-zA-Z])/g, (e) => {
+        if (e.length === 1) {
+            return e.toUpperCase();
+        } else {
+            return e[1].toUpperCase();
+        }
+    });
+};
+function squareDigit(n) {
+    // return +[...(n + "")].reduce((a, b) => a + b * b, "");
+    return +[...(n + "")].map((a) => a * a).join("");
+}
+console.log(squareDigit(223));
+console.log("hello world from the country of Egypt".camelCase());
