2252232ab541768d4d7d41048c833765b9784555
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-18 01:07:29 +0200

    Update 81_code.js

--- 81_code.js
+++ 81_code.js
@@ -22,3 +22,26 @@ Array.prototype.myMap = function (callback) {
 };
 
 // console.log(ar.myMap((n) => n * 2));
+function sqr(n) {
+    if (n < 0 || typeof n === "string" || isNaN(n)) return NaN;
+    let left = 0;
+    let right = n;
+    let result;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (mid * mid === n) {
+            return mid;
+        } else if (mid * mid < n) {
+            left = mid + 1;
+            result = mid;
+        } else if (mid * mid > n) {
+            right = mid - 1;
+        }
+    }
+    return result;
+}
+console.log(sqr(0));
+
+console.log(Math.sqrt("hello"));
+console.log(Math.sqrt(NaN));
+console.log(Math.sqrt(-Infinity));
