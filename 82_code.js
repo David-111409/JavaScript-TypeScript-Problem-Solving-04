091152eb6379168cabaf8fc3edc558d660497024
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-18 13:34:19 +0200

    Update 82_code.js

--- 82_code.js
+++ 82_code.js
@@ -25,3 +25,16 @@ function validate(str) {
     return stack.length === 0;
 }
 console.log(validate("{[()]{}"));
+
+function flatten(ar, depth = 1) {
+    return ar.reduce((acc, cur) => {
+        if (Array.isArray(cur) && depth > 0) {
+            return acc.concat(flatten(cur, depth - 1));
+        }
+        depth = depth - 1;
+        return acc.concat(cur);
+    }, []);
+}
+
+console.log(flatten([[[2, 3], 4, 5, [[[4]]], [[[6]]]]], 2));
+console.log(flatten([1, [2], [3, [4]]], 1));
