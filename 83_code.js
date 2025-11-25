122ec1a0d9e7ef7f2d56df6d966f59209029bf4d
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-26 01:07:09 +0200

    Update 83_code.js

--- 83_code.js
+++ 83_code.js
@@ -6,3 +6,20 @@ console.log(removeEmptyArrays([1, 2, [], 4]), [1, 2, 4]);
 console.log(removeEmptyArrays([[], [], [], []]), []);
 console.log(removeEmptyArrays([1, 2, 3, 4]), [1, 2, 3, 4]);
 console.log(removeEmptyArrays([1, [], [], 4, 0, "", null, undefined, false]), [1, 4]);
+
+
+{
+let x = [1, 2, 3, 4, 5];
+
+function minusOne(ar) {
+    let result = [...ar];
+    result.pop();
+    return result;
+}
+
+console.log(minusOne(x));
+console.log(minusOne(x));
+console.log(minusOne(x));
+console.log(minusOne(x));
+console.log(minusOne(x));
+}
