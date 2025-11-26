39e5ba687f7a6e40f558610c94a32811464b890c
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-26 20:36:23 +0200

    Update 83_code.js

--- 83_code.js
+++ 83_code.js
@@ -56,3 +56,19 @@ console.log(calculateSum("mushroom"), 890);
 console.log(reverseString("Think"), "knihT");
 console.log(reverseString("world"), "dlrow");
 console.log(reverseString("people"), "elpoep");
+
+function hasValidPrice(product) {
+    return !!(
+        product &&
+        typeof product.price === "number" &&
+        product.price >= 0
+    );
+}
+
+console.log(hasValidPrice({ product: "Milk", price: 1.5 }), true);
+console.log(hasValidPrice({ product: "Cheese", price: -1 }), false);
+console.log(hasValidPrice({ product: "Eggs", price: 0 }), true);
+console.log(hasValidPrice({ product: "Flour" }), false);
+console.log(hasValidPrice({ product: "Cerials", price: "3.0" }), false);
+console.log(hasValidPrice({ product: "Beer", price: NaN }), false);
+console.log(hasValidPrice(), false);
