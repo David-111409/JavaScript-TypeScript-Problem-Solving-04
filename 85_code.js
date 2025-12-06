7cb7295e614e489f82265b308e0fea6ae17fafa2
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-06 20:02:20 +0200

    Create 85_code.js

---
+++ 85_code.js
@@ -0,0 +1,66 @@
+// console.log("Hello from try.js");
+
+// const user = {name: "Khaled", age: 30};
+// let name, age;
+
+// ({name, age} = user);
+// console.log(name, age);
+// const ar = [1, 2, 3, 4, 5];
+
+// const [first, , , , last] = ar;
+
+// console.log(first, last);
+
+/*
+  Destructuring
+  - Challenge
+*/
+
+let chosen = 3;
+
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
+
+// for (let obj of myFriends) {
+//     const {
+//         title,
+//         age,
+//         available,
+//         skills: [skill1, skill2],
+//     } = obj;
+//     console.log(
+//         "Title:",
+//         title,
+//         "Age:",
+//         age,
+//         "Available:",
+//         available,
+//         "His skills:",
+//         skill1,
+//         skill2
+//     );
+// }
+
+function showNumber(chosen) {
+    const {
+        title,
+        age,
+        available,
+        skills: [skill1, skill2],
+    } = myFriends[chosen - 1];
+    console.log(
+        "Title:",
+        title,
+        "\nAge:",
+        age,
+        available ? "\nAvailable" : "\nNot Available",
+        "\nHis skills:",
+        skill1,
+        skill2
+    );
+}
+
+showNumber(chosen);
