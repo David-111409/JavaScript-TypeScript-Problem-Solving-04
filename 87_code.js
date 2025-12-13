96b4d29bdb63f49f11bb9b2b780b3c4ba274d817
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-13 04:27:51 +0200

    Implement User class with static methods and count

--- 87_code.js
+++ 87_code.js
@@ -69,3 +69,82 @@ function generateScores() {
 }
 
 console.log(generateScores());
+
+// function getNameAndJob(name, job) {
+//     let _name = name;
+//     let _job = job;
+//     return {
+//         getName() {
+//             return _name;
+//         },
+//         getJob() {
+//             return _job;
+//         },
+//     };
+// }
+
+// // let person1 = getNameAndJob("John", "Developer");
+// // console.log(person1.getName()); // John
+// // console.log(person1.getJob()); // Developer
+
+// // let string = "Elzero web school";
+// // console.log(string.substring(string.length - 5, string.length - 3));
+// // console.log(string.substr(-5, 2));
+
+// let string = "Elzero Web School";
+// console.log(string.charAt(2).toUpperCase() + string.slice(3, 6));
+// console.log(string.split(" ", 1));
+// // let num = 10;
+// // let objNum = new Number(10);
+// // console.log(num.constructor , objNum.constructor);
+
+// let num = 10;
+// let objNum = new Number(10);
+// console.log(num.valueOf(), objNum.valueOf());
+
+// let str = "Elzero";
+// let objStr = new String("Elzero");
+// console.log(str.constructor, objStr.constructor === String);
+// console.log(
+//     str.valueOf(),
+//     objStr.valueOf(),
+//     str === objStr,
+//     str == objStr,
+//     str === objStr.valueOf()
+// ); // false true true
+
+// let arr = [1, 2, 3];
+// let objArr = new Array(1, 2, 3);
+// console.log(arr.constructor, objArr.constructor);
+// console.log(arr.valueOf(), objArr.valueOf());
+// console.log(arr === objArr, arr == objArr);
+
+// let obj1 = { name: "khaled" };
+// let obj2 = new Object({ name: "khaled" });
+// console.log(obj1.constructor, obj2.constructor);
+// console.log(obj1.valueOf(), obj2.valueOf());
+// console.log(obj1 === obj2, obj1 == obj2);
+
+class User {
+    static count = 0;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        User.count++;
+    }
+    static sayHello() {
+        return `Hello from the user class.`;
+    }
+    static sayCount() {
+        return `There are ${User.count} users made.`;
+    }
+}
+
+const user1 = new User("khaled", 20);
+const user2 = new User("khaled", 20);
+const user3 = new User("khaled", 20);
+const user4 = new User("khaled", 20);
+
+console.log(User.count);
+console.log(User.sayHello());
+console.log(User.sayCount());
