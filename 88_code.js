a8d14706d35b2e773d3f1183aafc9db230a796e3
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-16 21:44:42 +0200

    Update 88_code.js

--- 88_code.js
+++ 88_code.js
@@ -14,3 +14,90 @@ console.log(vowelLinks("this is not a drill"), false);
 console.log(vowelLinks("Patrice Evra"), true);
 console.log(vowelLinks("please log on to edabit"), true);
 console.log(vowelLinks("the quick brown fox was sad"), false);
+
+/*
+testFairness([[4, 3], [2, 4], [1, 2]],
+[[6, 2], [4, 2], [1, 1], [1, 1]])
+➞ true
+
+// Agatha's pieces: [4, 3], [2, 4], [1, 2]
+// Bertha's pieces: [6, 2], [4, 2], [1, 1], [1, 1]
+
+// Total area of Agatha's chocolate
+// 4x3 + 2x4 + 1x2 = 12 + 8 + 2 = 22
+
+// Total area of Bertha's chocolate is:
+// 6x2 + 4x2 + 1x1 + 1x1 = 12 + 8 + 1 + 1 = 22
+*/
+
+function testFairness(ar1, ar2) {
+    return (
+        ar1.reduce((a, b) => a + b[0] * b[1], 0) ===
+        ar2.reduce((a, b) => a + b[0] * b[1], 0)
+    );
+}
+
+console.log(
+    testFairness(
+        [
+            [1, 5],
+            [6, 3],
+            [1, 1],
+        ],
+        [
+            [7, 1],
+            [2, 2],
+            [1, 1],
+        ]
+    ),
+    false
+);
+console.log(
+    testFairness(
+        [
+            [2, 2],
+            [2, 2],
+            [2, 2],
+            [2, 2],
+        ],
+        [[4, 4]]
+    ),
+    true
+);
+console.log(
+    testFairness(
+        [
+            [1, 2],
+            [2, 1],
+        ],
+        [[2, 2]]
+    ),
+    true
+);
+console.log(
+    testFairness(
+        [
+            [1, 2],
+            [2, 1],
+        ],
+        [
+            [2, 2],
+            [4, 4],
+        ]
+    ),
+    false
+);
+console.log(
+    testFairness(
+        [
+            [2, 4],
+            [1, 1],
+            [11, 1],
+        ],
+        [
+            [2, 2],
+            [4, 4],
+        ]
+    ),
+    true
+);
