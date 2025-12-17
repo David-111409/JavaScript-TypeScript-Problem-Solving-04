51fb4423d42a5c43526f2076a24ea92e6aa0c455
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-18 00:27:40 +0200

    Update 89_code.js

--- 89_code.js
+++ 89_code.js
@@ -25,3 +25,31 @@ console.log(counter());
 console.log(counter());
 console.log(counter());
 console.log(counter());
+
+let ar = [2, 10, 2, 0, 10, -1, 0, 2, 10, 8, 200];
+
+function removeZeros(ar) {
+    // for (let i = ar.length - 1; i >= 0; i--) {
+    //     if (ar[i] === 0) {
+    //         ar.splice(i, 1);
+    //         ar.push(0);
+    //     }
+    // }
+    // return ar;
+    let pointer = 0;
+    for (let i = 0; i < ar.length ; i++){
+        if (ar[i] !== 0) {
+            ar[pointer] = ar[i];
+            pointer++;
+        }
+    }
+
+    while (pointer < ar.length){
+        ar[pointer] = 0;
+        pointer++;
+    }
+    return ar
+}
+
+removeZeros(ar);
+console.log(ar);
