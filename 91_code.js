42b9df87716a3c905e46eaf0c17557c9db53533f
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-25 14:05:09 +0200

    Update 91_code.js

--- 91_code.js
+++ 91_code.js
@@ -10,3 +10,53 @@ const getActiveUserNames = (users) =>
         return acc;
     }, []); // ['Ali', 'John']Names
 console.log(getActiveUserNames(users));
+
+// let job = "tester";
+
+// let salary = 0;
+
+// switch (job) {
+//     case "manager":
+//         salary = 100000;
+//         break;
+//     case "developer":
+//         salary = 80000;
+//         break;
+//     case "tester":
+//         salary = 50000;
+//         break;
+//     default:
+//         salary = 0;
+//         break;
+// }
+
+// console.log("The salary of " + job + " is " + salary);
+
+let holdiays = 2;
+let money = 0;
+
+switch (holdiays) {
+    case 0:
+        money = 20000;
+        break;
+    case 1:
+    case 10:
+        money = 10000;
+        break;
+    case 2:
+        money = 40;
+        break;
+    default:
+        money = 0;
+        break;
+}
+console.log("Money: " + money);
+
+if (holdiays === 0) {
+    money = 20000;
+} else if (holdiays === 1 || holdiays === 10) {
+    money = 10000;
+} else if (holdiays === 2) {
+    money = 40;
+}
+console.log("Money: " + money);
