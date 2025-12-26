35685f62d55e40721ac1ae80bbe04e42f4fd5369
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-12-27 01:38:27 +0200

    Update 93_code.js

--- 93_code.js
+++ 93_code.js
@@ -23,3 +23,20 @@ let ar = [1, 2, 3, 4, 5];
 reverseArray(ar);
 console.log(ar);
 // output → [4, 3, 2, 1]
+
+// const name = "dawoud";
+// const number = "1234";
+// console.log(Array.from(name, (ch) => ch.toUpperCase()));
+// console.log(Array.from(number, (ch) => ch * 2));
+// const ar = [1, 2, 3, 4, 5];
+// const newAr = ar.with(-1, "hello");
+// const newAr = [...ar];
+// newAr[1] = "hello";
+// console.log(ar, newAr);
+// console.log(ar.at(-1), ar.at(0), ar.at(50));
+
+// const url = "https://dummyjson.com/users";
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
