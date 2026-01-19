f460d46ab6ead8d45662954897d10a46ab0787d4
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2026-01-19 11:07:24 +0200

    Update 97_code.ts

--- 97_code.ts
+++ 97_code.ts
@@ -19,3 +19,71 @@ const findPeaks = (ar: number[]): number[] => {
 };
 
 console.log(findPeaks([1, 5, 5, 2]));
+
+
+// type obj = {
+//   id: number;
+//   title: string;
+// };
+
+class BankAccount {
+  //   username?: string;
+  constructor(
+    public readonly accountNumber: number,
+    private balance: number,
+    public username?: string
+  ) {
+    this.username = username;
+  }
+
+  deposit(amount: number): void {
+    if (amount <= 0) {
+      throw new Error("The amount should be positive.");
+    }
+    this.balance += amount;
+  }
+
+  withdraw(amount: number): void {
+    if (amount <= 0) {
+      throw new Error("The amount should be positive.");
+    } else if (amount > this.balance) {
+      throw new Error("The balance is not sufficient.");
+    }
+
+    this.balance -= amount;
+  }
+
+  get getBalance(): number {
+    return this.balance;
+  }
+
+  set giveBalance(amount: number) {
+    if (amount <= 0) {
+      throw new Error("The balance should be positve");
+    }
+    this.balance = amount;
+  }
+}
+
+// const account = new BankAccount(200);
+// account.deposit(100);
+// account.withdraw(-10);
+// console.log(account.getBalance());
+
+const myAccount = new BankAccount(2, 50, "dawoud");
+
+myAccount.deposit(50);
+console.log(myAccount.getBalance, myAccount.accountNumber, myAccount.username); // Expected: 150
+
+// myAccount.withdraw(200); // Expected: "Inssuficient funds"
+
+myAccount.withdraw(30);
+console.log(myAccount.getBalance); // Expected: 120
+
+// myAccount.giveBalance = -500;
+// console.log(myAccount.getBalance);
+// IMPORTANT: This should cause a TypeScript error if you try it:
+// myAccount.balance = 5000;
+
+console.log(myAccount.constructor === BankAccount);
+console.log(myAccount instanceof BankAccount);
